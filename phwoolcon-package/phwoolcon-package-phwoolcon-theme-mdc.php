bb610871ced067a464e696fc8a407ed92c36b7af
Fishdrowned <fish.drowned@gmail.com>
2017-09-11 03:36:01 +0800

    feat: add normalize-7.0.0.min.css

--- phwoolcon-package/phwoolcon-package-phwoolcon-theme-mdc.php
+++ phwoolcon-package/phwoolcon-package-phwoolcon-theme-mdc.php
@@ -4,6 +4,7 @@ return [
     'phwoolcon/theme-mdc' => [
         'assets' => [
             'mdc-css' => [
+                'normalize/normalize-7.0.0.min.css',
                 'material-components-web-0.20.0/material-components-web.min.css',
             ],
             'mdc-js' => [
