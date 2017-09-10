dd2c936bbe754609e3e45b8e295131ae7554f4ce
Fishdrowned <fish.drowned@gmail.com>
2017-09-10 15:52:38 +0800

    refactor: remove useless files

--- phwoolcon-package/phwoolcon-package-phwoolcon-theme-mdc.php
+++ phwoolcon-package/phwoolcon-package-phwoolcon-theme-mdc.php
@@ -2,21 +2,6 @@
 
 return [
     'phwoolcon/theme-mdc' => [
-        'di' => [
-            20 => 'di.php', // 20 stands for the loading sequence, bigger number will be loaded later
-        ],
-        'commands' => [
-            // 20 stands for the loading sequence, bigger number will be loaded later
-            20 => [
-//                'your:command' => Phwoolcon\ThemeMdc\Command\YourCommand::class,
-            ],
-        ],
-        'class_aliases' => [
-            // 20 stands for the loading sequence, bigger number will be loaded later
-            20 => [
-//                'ShortClass' => Phwoolcon\ThemeMdc\Some\LongClassName::class,
-            ],
-        ],
         'assets' => [
             'mdc-css' => [
                 'mdc-0.20.0/mdc.css',
