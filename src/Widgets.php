1916a40b852ea8130e3bf3e5730c258f2d6ec356
Christopher CHEN <fishdrowned@gmail.com>
2017-11-02 19:34:21 +0800

    feat(widget): add required field to `mdcCheckbox`

--- src/Widgets.php
+++ src/Widgets.php
@@ -23,10 +23,11 @@ class Widgets extends Widget
         $value = isset($htmlAttributes['value']) ?: '';
         $checked = empty($htmlAttributes['checked']) ? '' : 'checked';
         $name = empty($htmlAttributes['name']) ? '' : 'name="' . $htmlAttributes['name'] . '"';
+        $required = empty($htmlAttributes['required']) ? '' : 'required';
         return <<<HTML
 <div class="mdc-form-field">
     <div class="mdc-checkbox">
-        <input type="checkbox" id="{$id}" {$name} class="mdc-checkbox__native-control" value="{$value}" {$checked}>
+        <input type="checkbox" id="{$id}" {$name} class="mdc-checkbox__native-control" value="{$value}" {$checked} {$required}>
         <div class="mdc-checkbox__background">
             <svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24">
                 <path class="mdc-checkbox__checkmark__path" fill="none" stroke="white"
